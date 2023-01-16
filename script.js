a04def78eea6b65aa301838d5fc6ef8869b46a8e
Sarah Bridgeman <skbridgeman@hotmail.com>
2023-01-15 19:24:12 -0500

    Add button options

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const button = document.querySelector('button');
-const container = document.querySelector('.container');
-
+//Determines grid size by prompt.//
 function makeGrid() {
-    let gridSize = prompt("How many squares per side is the grid?");
-    if (gridSize > 100 || gridSize < 1) {
-        alert("Enter a number between 1 and 100.")
-    }
+    const container = document.querySelector('.container');
+    let gridSize = prompt("How many squares per side in the sketchpad? Enter a number 1 to 100.");
+    if (gridSize < 1 || gridSize > 100) {
+        alert("Can't make sketchpad. Try again.");
+    } else {
     for (let i = 0; i < gridSize; i++) {
         const innerContainer = document.createElement('div');
         innerContainer.classList.add('inner-container');
@@ -14,11 +13,39 @@ function makeGrid() {
             const square = document.createElement('div');
             square.classList.add('square');
             innerContainer.appendChild(square);
+
+            //Black & White button.//
+            const bw = document.querySelector('.bw');
+            bw.addEventListener('click', function (e) {
             square.addEventListener('mouseover', function (e) {
                 square.style.cssText = 'background-color: black;'
+                }); 
             });
-        }       
-    }  
-}    
+            
+            //Picks random color.//
+            const random = () => Math.floor(Math.random() * 256);
+            const r = random ();
+            const g = random ();
+            const b = random ();
+            const rgb = 'rgb(' + r + ',' + g + ',' + b +')';
+            
+            //Rainbow button.//
+            const rainbow = document.querySelector('.rainbow');
+            rainbow.addEventListener('click', function(e) {
+            square.addEventListener('mouseover', function (e) {
+                square.style.backgroundColor = rgb;
+                }); 
+            });
+            
+            //Eraser button.//
+            const eraser = document.querySelector('.eraser');
+            eraser.addEventListener('click', function (e) {
+            square.addEventListener('mouseover', function (e) {
+                square.style.cssText = 'background-color: white;'
+                }); 
+            });
 
-
+        }      
+    }}  
+} 
+makeGrid(); 
